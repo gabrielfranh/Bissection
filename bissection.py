0d745a00477e2e85eb2d0d9e29c210539587ec07
Gabriel F. Habermann <gabrielfranh@gmail.com>
2019-09-26 20:18:17 -0300

    First commit - Adding the program

---
+++ bissection.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# Algoritmo Método da Bisseção
+
+# Gabriel Francisco Habermann  
+
+import math    
+
+def funcao (inicial):
+
+   return pow(inicial,3) - (9 * inicial) + 3
+
+a = float(input("Começo do intervalo: "))
+
+b = float(input("Final do intervalo b: "))
+
+precisao = float(input("Precisão: "))
+
+print ('\n')
+
+if (b - a ) < precisao:
+
+   resultado = a
+
+else:
+
+   contador = 1
+
+   F = funcao(a)
+
+   while True:
+
+      x = (a + b)/2
+
+      print "Iteração: %d" % (contador)
+
+      print "Valor de X: %f" % (x)
+
+      print '\n'
+
+      if F * funcao(x) > 0:
+
+         a = x
+
+      else:
+
+         b = x
+
+      if (b - a) <= precisao:
+
+         resultado = (a + b)/2
+
+         break
+
+      contador = contador + 1
+
+      
+
+print 'Valor do resultado: %f' % (resultado)
